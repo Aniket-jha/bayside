dd8ad3ac441983d5d497ba0bbe2c224b8239e631
Aniket Jha <aniketjha@Anikets-Laptop.local>
2024-07-23 20:45:50 +0530

    First Commit

---
+++ src/Components/galleryData.js
@@ -0,0 +1,275 @@
+import BaysideMainImage from "../asset/BaysideOne.webp"
+import BaysideOneImage from "../asset/canteen.webp"
+import BaysideTwoImage from "../asset/BaysideTwo.webp"
+import BaysideThreeImage from "../asset/BaysideThree.webp"
+import BaysideFourImage from "../asset/BaysideFour.webp"
+import One from "../asset/twin.webp"
+import Two from "../asset/private.webp"
+import Three from "../asset/bayside21.webp"
+import Four from "../asset/643477448d0e68d5cb4ff2de9e3fbf2259eed220-700x500 (1).webp"
+import Five from "../asset/8f72ec06cc8e62df423d1db557dda63dac795b0a-700x500 (1).webp"
+import EventOne from "../asset/event/event1.webp"
+import EventTwo from "../asset/event/event2.webp"
+import EventThree from "../asset/event/event3.webp"
+import EventFour from "../asset/event/event4.webp"
+import EventFive from "../asset/event/event5.webp"
+import EventSix from "../asset/event/event6.webp"
+import EventSeven from "../asset/event/event7.webp"
+import EventEight from "../asset/event/event8.webp"
+import EventNine from "../asset/event/event9.webp"
+import EventTen from "../asset/event/event10.webp"
+import EventEleven from "../asset/event/event11.webp"
+import EventTwelve from "../asset/event/event12.webp"
+import EventThirteen from "../asset/event/event13.webp"
+import EventMainOne from "../asset/event/eventmainone.webp"
+import EventMainThree from "../asset/event/eventmainthree.webp"
+import EventVideoOne from "../asset/event/videoone.mp4"
+import EventVideoTwo from "../asset/event/video2.mp4"
+import EventVideoThree from "../asset/event/video3.mp4"
+import EventVideoFour from "../asset/event/video4.mp4"
+import EventVideoFive from "../asset/event/video5.mp4"
+import EventVideoSix from "../asset/event/video6.mp4"
+import EventVideoSeven from "../asset/event/video7.mp4"
+import EventVideoEight from "../asset/event/video8.mp4"
+export const galleryImages = [
+            
+          
+    {
+        src: BaysideMainImage,
+        width: "100%",
+        height: "100%",
+      },
+      {
+        src: BaysideOneImage,
+        width: "100%",
+        height: "100%",
+      },
+      {
+        src: BaysideTwoImage,
+        width: "100%",
+        height: "100%",
+      },
+      {
+        src: BaysideThreeImage,
+        width: "100%",
+        height: "100%",
+      },
+    
+   
+      {
+        src: One,
+        width: "100%",
+        height: "100%",
+      },
+      {
+        src: Two,
+        width: "100%",
+        height: "100%",
+      },
+      {
+        src:Three,
+        width: "100%",
+        height: "100%",
+      },
+      {
+        src:Four,
+        width: "100%",
+        height: "100%",
+      },
+      {
+        src: Five,
+        width: "100%",
+        height: "100%",
+      },
+     
+      
+      
+]
+
+
+export const eventData = [
+  {
+    src: EventMainOne,
+    width: "100%",
+    height: "100%",
+  },
+  {
+    src: EventMainThree,
+    width: "100%",
+    height: "100%",
+  },
+  {
+    src: EventThirteen,
+    width: "100%",
+    height: "100%",
+  },
+  {
+    type: "video",
+    width: "100%",
+    height: "100%",
+    
+    sources: [
+      {
+        src: EventVideoOne,
+        type: "video/mp4",
+      },
+    ],
+  },
+  {
+    src: EventOne,
+    width: "100%",
+    height: "100%",
+  },
+  {
+    src: EventTwo,
+    width: "100%",
+    height: "100%",
+  },
+  {
+    src: EventThree,
+    width: "100%",
+    height: "100%",
+  },
+  {
+    type: "video",
+    width: "100%",
+    height: "100%",
+    
+    sources: [
+      {
+        src: EventVideoSeven,
+        type: "video/mp4",
+      },
+    ],
+  },
+  {
+    src: EventFour,
+    width: "100%",
+    height: "100%",
+  },
+  {
+    type: "video",
+    width: "100%",
+    height: "100%",
+    
+    sources: [
+      {
+        src: EventVideoSix,
+        type: "video/mp4",
+      },
+    ],
+  },
+  {
+    src: EventFive,
+    width: "100%",
+    height: "100%",
+  },
+  {
+    type: "video",
+    width: "100%",
+    height: "100%",
+    
+    sources: [
+      {
+        src: EventVideoTwo,
+        type: "video/mp4",
+      },
+    ],
+  },
+  {
+    src: EventSix,
+    width: "100%",
+    height: "100%",
+  },
+  {
+    src: EventTwelve,
+    width: "100%",
+    height: "100%",
+  },
+  {
+    src: EventSeven,
+    width: "100%",
+    height: "100%",
+  },
+  {
+    type: "video",
+    width: "100%",
+    height: "100%",
+    
+    sources: [
+      {
+        src: EventVideoSix,
+        type: "video/mp4",
+      },
+    ],
+  },
+  {
+    src: EventEight,
+    width: "100%",
+    height: "100%",
+  },
+  {
+    type: "video",
+    width: "100%",
+    height: "100%",
+    
+    sources: [
+      {
+        src: EventVideoThree,
+        type: "video/mp4",
+      },
+    ],
+  },
+  {
+    src: EventNine,
+    width: "100%",
+    height: "100%",
+  },
+  {
+    type: "video",
+    width: "100%",
+    height: "100%",
+    
+    sources: [
+      {
+        src: EventVideoFour,
+        type: "video/mp4",
+      },
+    ],
+  },
+  {
+    src: EventTen,
+    width: "100%",
+    height: "100%",
+  },
+  {
+    type: "video",
+    width: "100%",
+    height: "100%",
+    
+    sources: [
+      {
+        src: EventVideoFive,
+        type: "video/mp4",
+      },
+    ],
+  },
+  {
+    src: EventEleven,
+    width: "100%",
+    height: "100%",
+  },
+  {
+    type: "video",
+    width: "100%",
+    height: "100%",
+    
+    sources: [
+      {
+        src: EventVideoEight,
+        type: "video/mp4",
+      },
+    ],
+  },
+  
+]
